2ddc0b84c2005ec7bf8a9ad8555b4f05d6fe7b68
Joao Victor Santos <joaovictorsantoscpv@gmail.com>
2020-09-01 10:54:45 -0300

    chore: create initial structure

---
+++ android/app/src/main/java/com/search_movies_and_series/MainActivity.java
@@ -0,0 +1,15 @@
+package com.search_movies_and_series;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "search_movies_and_series";
+  }
+}
