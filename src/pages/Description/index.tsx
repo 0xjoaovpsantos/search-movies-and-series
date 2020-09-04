c53c027216a685409b0416ba76ebdde1f4b105cb
joaovictorpsantos <joaovictorsantoscpv@gmail.com>
2020-09-03 21:14:53 -0300

    chore: stylize Description page

--- src/pages/Description/index.tsx
+++ src/pages/Description/index.tsx
@@ -1,6 +1,42 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { Image, StyleSheet } from 'react-native';
+import { Container, WrapperPoster, Title, Year, Text } from './styles';
+import { ScrollView } from 'react-native-gesture-handler';
 
-const Description: React.FC = () => <Text>Description page</Text>;
+const Description: React.FC = () => (
+  <Container>
+    <ScrollView>
+      <WrapperPoster>
+        <Image
+          source={{
+            uri:
+              'https://images-na.ssl-images-amazon.com/images/I/71niXI3lxlL._AC_SY741_.jpg',
+          }}
+          style={styles.img}
+        />
+
+        <Title>Avengers EndGame</Title>
+        <Year>2019 - 73 min</Year>
+      </WrapperPoster>
+      <Text>
+        "When the Chitauri invaders are sighted in the African kingdom of
+        Wakanda, the Avengers covertly enter the advanced nation to
+        investigate."
+      </Text>
+      <Text>Director</Text>
+      <Text>Writers</Text>
+      <Text>Actos</Text>
+    </ScrollView>
+  </Container>
+);
+
+const styles = StyleSheet.create({
+  img: {
+    width: 200,
+    resizeMode: 'stretch',
+    height: 300,
+    borderRadius: 4,
+  },
+});
 
 export default Description;
---
+++ src/pages/Description/styles.ts
@@ -0,0 +1,29 @@
+import styled from 'styled-components/native';
+import theme from '../../utils/theme';
+
+export const Container = styled.View`
+  flex: 1;
+  background-color: ${theme.background};
+  padding: 20px;
+`;
+
+export const WrapperPoster = styled.View`
+  align-items: center;
+  margin: 40px;
+`;
+
+export const Title = styled.Text`
+  color: ${theme.font};
+  font-size: 24px;
+  font-weight: bold;
+`;
+
+export const Year = styled.Text`
+  color: ${theme.font};
+`;
+
+export const Text = styled.Text`
+  color: ${theme.font};
+  line-height: 24px;
+  margin: 10px 0;
+`;
